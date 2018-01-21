d2ee33156edf98d05f005c2621a346876cecbc93
Richard Tweed <RichardoC@users.noreply.github.com>
2018-01-21 12:06:17 +0000

    flaskExample.py now checks if transfer happened

--- flaskExample.py
+++ flaskExample.py
@@ -25,29 +25,28 @@ def sms_reply():
         fromID = parts[0]
         toID = parts[1]
         amount = parts[2]
-        ledger.transfer(fromID, toID, amount)
-        try:
-            ledger.transfer(fromID, toID, amount)
-            m = '{0} -> {1} -> ${2}'.format(fromID,
-                                            toID,
-                                            amount)
-        except:
+        status = ledger.transfer(fromID, toID, amount)
+        if status:
+            m = ':)'
+        else:
             m = ':('
         resp.message(m)
+    
     elif len(parts) == 4:
         m = 'Error'
         if parts[1] == '->':
-            fromID = parts[0]
-            toID = parts[2]
-            amount = parts[3]
-            try:
-                ledger.transfer(fromID, toID, amount)
+           fromID = parts[0]
+           toID = parts[2]
+           amount = parts[3]
+           status = ledger.transfer(fromID, toID, amount)
+           if status:
                 m = '{0} -> {1} -> ${2}'.format(fromID,
-                                                toID,
-                                                amount)
-            except:
-                m = ':('
-        resp.message(m)
+                                               toID,
+                                               amount)
+            else:
+               m = ':('
+           resp.message(m)
+
     elif len(parts) == 1:
         #try balance
         #send back balance
@@ -55,6 +54,7 @@ def sms_reply():
         resp.message(bal)
     else:
         resp.message("Error")
+    
     return str(resp)
 
 @app.route("/")
@@ -62,4 +62,4 @@ def hello():
     return "Default Hello World!"
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+app.run(host='0.0.0.0')
