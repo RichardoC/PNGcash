9938e10c237dcc58caa544c3e2ee0142cd56e7c2
Richard Tweed <RichardoC@users.noreply.github.com>
2018-01-21 13:40:08 +0000

    int everything

--- flaskExample.py
+++ flaskExample.py
@@ -22,9 +22,9 @@ def sms_reply():
 
     # Determine the right reply for this message
     if len(parts) == 3:
-        fromID = parts[0]
-        toID = parts[1]
-        amount = parts[2]
+        fromID = int(parts[0])
+        toID = int(parts[1])
+        amount = int(parts[2])
         status = ledger.transfer(fromID, toID, amount)
         if status:
             m = ':) \n{0} -> {1} -> ${2}'.format(fromID,
@@ -36,9 +36,9 @@ def sms_reply():
     elif len(parts) == 4:
         m = 'Error'
         if parts[1] == '->':
-           fromID = parts[0]
-           toID = parts[2]
-           amount = parts[3]
+           fromID = int(parts[0])
+           toID = int(parts[2])
+           amount = int(parts[3])
            status = ledger.transfer(fromID, toID, amount)
            if status:
                m = '{0} -> {1} -> ${2}'.format(fromID,
@@ -49,7 +49,7 @@ def sms_reply():
            resp.message(m)
     elif len(parts) == 1:
         # Check balance
-        bal = ledger.balance(parts[0])
+        bal = ledger.balance(int(parts[0]))
         resp.message(bal)
     else:
         resp.message("Error")
